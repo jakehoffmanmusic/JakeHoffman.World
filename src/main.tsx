72986cadb215ad52308accfcb814301211993151
Jake Hoffman <jakehoffmanmusic@gmail.com>
2026-02-28 20:05:53 -0500

    Force fresh build to fix live site styling

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/App.css'
+// Cache bust: 1772153862364
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
